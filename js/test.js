6030a7e6ac870784dc66f05bb102f56b125ef103
admin <0312rachelo@gmail.com>
2024-04-24 16:31:43 +0900

    beforemiddle

--- js/test.js
+++ js/test.js
@@ -21,8 +21,17 @@ if (true) {
   //const f // Error?
   console.log(e);
 
-  document.getElementById("search_btn").addEventListener('click', search_message);
 
-function search_message(){
-  alert("검색을 수행합니다!");
-}
+
+//function search_message(){
+//  alert("검색을 수행합니다!");
+//}
+
+
+
+const search_message = () => {
+  const c = '검색을 수행합니다.';
+  alert(c);
+  } 
+
+  document.getElementById("search_btn").addEventListener('click', search_message);
