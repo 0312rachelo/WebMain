f377632d77cff09fc7aea87b94140b43d897e144
admin <0312rachelo@gmail.com>
2024-05-29 16:53:28 +0900

    7

--- js/logout.js
+++ js/logout.js
@@ -35,18 +35,6 @@ function getCookie(name) {
     return ;
 }
 
-function init(){ // 로그인 폼에 쿠키에서 가져온 아이디 입력
-    const emailInput = document.getElementById('typeEmailX');
-    const idsave_check = document.getElementById('idSaveCheck');
-    let get_id = getCookie("id");
-    
-    if(get_id) {
-    emailInput.value = get_id;
-    idsave_check.checked = true;
-    }
-    session_check();
-}
-
 function session_set() { //세션 저장
     let session_id = document.querySelector("#typeEmailX");
     let session_pass = document.querySelector("#typePasswordX");
@@ -86,11 +74,8 @@ function session_del() {
 
 function logout() {
     session_del();
-    location.href='../index.html';
+    location.href= "../index.html"
 }
-
-
-
 
 
 function encodeByAES256(key, data){
@@ -136,81 +121,5 @@ function init_logined(){
     }
 }
 
-const check_input = () => {
-    
-    const idsave_check = document.getElementById('idSaveCheck');
-    const loginForm = document.getElementById('login_form');
-    const loginBtn = document.getElementById('login_btn');
-    const emailInput = document.getElementById('typeEmailX');
-    const passwordInput = document.getElementById('typePasswordX');
-    const c = '아이디, 패스워드를 체크합니다';
-    alert(c);
-    const emailValue = emailInput.value.trim();
-    const passwordValue = passwordInput.value.trim();
 
-    if (emailValue === '') {
-    alert('이메일을 입력하세요.');
-    return false;
-    }
-
-    if (passwordValue === '') {
-    alert('비밀번호를 입력하세요.');
-    return false;
-    }
-
-    if (emailValue.length < 5) {
-        alert('아이디는 최소 5글자 이상 입력해야 합니다.');
-        return false;
-        }
-        if (passwordValue.length < 12) {
-        alert('비밀번호는 반드시 12글자 이상 입력해야 합니다.');
-        return false;
-        }
-        const hasSpecialChar = passwordValue.match(/[!,@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/) !== null;
-        if (!hasSpecialChar) {
-        alert('패스워드는 특수문자를 1개 이상 포함해야 합니다.');
-        return false;
-        }
-        const hasUpperCase = passwordValue.match(/[A-Z]+/) !== null;
-        const hasLowerCase = passwordValue.match(/[a-z]+/) !== null;
-        if (!hasUpperCase || !hasLowerCase) {
-        alert('패스워드는 대소문자를 1개 이상 포함해야 합니다.');
-        return false;
-        }
-
-        const sanitizedPassword = check_xss(passwordValue);
-        // check_xss 함수로 비밀번호 Sanitize
-            const sanitizedEmail = check_xss(emailValue);
-        // check_xss 함수로 비밀번호 Sanitize
-            if (!sanitizedEmail) {
-        // Sanitize된 비밀번호 사용
-            return false;
-            }
-        
-            if (!sanitizedPassword) {
-        // Sanitize된 비밀번호 사용
-            return false;
-            }
-        
-
-    console.log('이메일:', emailValue);
-    console.log('비밀번호:', passwordValue);
-
- 
-    if(idsave_check.checked == true) { // 아이디 체크 o
-        alert("쿠키를 저장합니다.", emailValue);
-        setCookie("id", emailValue, 1); //1일 저장
-        alert("쿠키 값 :" + emailValue);
-        }
-        else
-        {//아이디 체크 x
-        setCookie("id", emailValue.value, 0); //날짜를 0 - 쿠키 삭제
-        session_set();
-        }
-    loginForm.submit();
-
-    };
-
-    document.getElementById("login_btn").addEventListener('click', check_input);
-
-      
+    document.getElementById("logout_btn").addEventListener('click', check_input);
--- js/pop_up.js
+++ js/pop_up.js
@@ -1,17 +1,17 @@
-function pop_up(){
-    var cookiecheck = getCookie("popupYN");
-        if (cookiecheck != "N"){
+function pop_up(){ //쿠키 검사
+    var cookiecheck = getCookie("popupYN"); //쿠키 검사
+        if (cookiecheck != "N"){ //쿠키 값에 n이 없는 경우, 없다면
             window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
         }
 }
 
-function setCookie(name, value, expiredays) {
+function setCookie(name, value, expiredays) { // 쿠키를 세팅하는 함수, expiredays: 키 만료 시간
     var date = new Date();
-    date.setDate(date.getDate() + expiredays);
+    date.setDate(date.getDate() + expiredays); // setdate: 날짜또는시간 설정, getdate: utc 표준 날짜 리턴
     document.cookie = escape(name) + "=" + escape(value) + "; expires=" + date.toUTCString() + "; path=/"; + ";SameSite=None; Secure";
-}
+} // >>/<< 이 세팅이 있어야 쿠키가 만들어짐
 
-function getCookie(name) {
+function getCookie(name) { // 쿠키를 가져오는 함수(쿠키가 존재한다면)
     var cookie = document.cookie;
     console.log("쿠키를 요청합니다.");
     if (cookie != "") {
@@ -19,8 +19,8 @@ function getCookie(name) {
         for ( var index in cookie_array) {
             var cookie_name = cookie_array[index].split("=");
 
-            if (cookie_name[0] == "popupYN") {
-                return cookie_name[1];
+            if (cookie_name[0] == "popupYN") { // popupYN값을 찾아서, cookie array 배열 반복함
+                return cookie_name[1]; //참고: 쿠키는 키, 값으로 이루어짐// 즉, 값은 인덱스 [1]이 된다
             }
         }
     }
@@ -55,11 +55,11 @@ function over(obj) {
 function out(obj) {
     obj.src="image/LOGO_2.png";}
 
-    function closePopup() {
+    function closePopup() { //체크 박스 클릭 시 윈도우를 닫는 함수, id 값이 존재할 시 클릭 이후에 쿠키를 세팅
         if (document.getElementById('check_popup').value) {
             setCookie("popupYN", "N", 1);
             console.log("쿠키를 설정합니다.");
-            self.close();
+            self.close(); //이걸로 현재 보고 있는 창 자신을 닫는다.
         }
     }
 
